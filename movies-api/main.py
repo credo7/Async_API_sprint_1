5f2c889bcf776b4461917677417b89470e451954
Chingis <chingisdev@gmail.com>
2023-08-15 19:53:15 +0300

    Delete direct uvicorn call

--- movies-api/main.py
+++ movies-api/main.py
@@ -37,8 +37,3 @@ async def shutdown():
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
-
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app', host='0.0.0.0', port=8000,
-    )

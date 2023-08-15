680ecc74081fe146016b36585189f519cff0bb86
Chingis <chingisdev@gmail.com>
2023-08-15 19:55:19 +0300

    Add waiting func
    - cleans code

--- etl/docker-entrypoint.sh
+++ etl/docker-entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Get the PostgreSQL user role from an environment variable
-POSTGRES_USER_ROLE=${POSTGRES_USER:-app}
-echo "Postgres user role is: $POSTGRES_USER_ROLE"
+# Function to wait for a service to be available
+wait_for_service() {
+  host="$1"
+  port="$2"
 
-# Wait for PostgreSQL
-until pg_isready -h postgres_test -U $POSTGRES_USER_ROLE; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+  echo "Waiting for $host:$port..."
 
-echo "Postgres is up - proceeding"
+  while ! nc -z "$host" "$port"; do
+    sleep 1
+  done
+
+  echo "$host:$port is available."
+}
+
+wait_for_service postgres 5432
+wait_for_service elasticsearch 9200
+wait_for_service redis 6379
 
 # Load data from SQLite to Postgres
 python sqlite_to_postgres/load_data.py
@@ -19,10 +25,5 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Wait for ElasticSearch to be ready
-while ! curl -s http://elasticsearch_test:9200/_cat/health; do
-    echo "Elastic is unavailable - sleeping"
-    sleep 1
-done
 
 python main.py
